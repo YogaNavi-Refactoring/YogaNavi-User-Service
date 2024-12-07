e9415e561ab85657b482decbdd10de215fbf0014
seojangwon <seojang980510@naver.com>
2024-12-07 20:31:32 +0900

    feature: [be] user 서비스 업로드

---
+++ src/main/java/com/yoganavi/user/YogaNaviUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yoganavi.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaNaviUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaNaviUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoganavi/user/YogaNaviUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoganavi.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogaNaviUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
